f76c262d3eb6a138f18b589dfdb09c7aa935e1ca
Space Ranger <40188223+jburrkellam@users.noreply.github.com>
2018-09-10 23:24:52 -0400

    Third commit

--- Py_bank/main.py
+++ Py_bank/main.py
@@ -1,11 +1,41 @@
 import os
 import csv
 
-csvpath= os.path.join('..', 'Resources', 'budget_data.csv')
+mainfile= os.path.join("Resources", "budget_data.csv")
+print(mainfile)
 
-with open(csvpath, 'r') as file_handler:
-    lines = file_handler.read()
-    print(lines)
-    print(type(lines))
+sum = 0
+count = 0
 
-    
+
+with open(mainfile) as openfile:
+    print(openfile)
+
+    budgetdata = csv.reader(openfile)
+    print(budgetdata)
+
+    header = next(budgetdata)
+    print(budgetdata)
+
+    for row in budgetdata:
+        count += 1
+        print(count)
+        
+        sum = sum + int(row[1])
+        print(sum)
+
+
+
+
+
+
+
+
+
+# Final Result = some sort of function(calculation)
+# # # Your task is to create a Python script that analyzes the records to calculate each of the following:
+# The total number of months included in the dataset
+# The total net amount of "Profit/Losses" over the entire period
+# The average change in "Profit/Losses" between months over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
